f66fcc18d01593e90b0ca7c01fd2a7e76e4bbc5b
supertopoz <deepsleepsounds@gmail.com>
2018-01-01 06:19:21 +0900

    add mobile menu and logo

--- src/app.js
+++ src/app.js
@@ -2,18 +2,40 @@ let menu = [
   'Apps-script',
   'JQuery',
   'Firebase',
-  'Redux'
+  'Redux',
+  'more',
+  'more',
+  'Apps-script',
+  'JQuery',
+  'Firebase',
+  'Redux',
+  'more',
+
 
 ]
 
 $('document').ready(()=>{
-	console.log('working')
+
   menu.forEach(item => {
 	$('#menu-buttons').append(
     '<div class="menu-button">'+item+'</div>'
     )
+    $('#menu-buttons-mini').append(
+    '<div class="menu-button">'+item+'</div>'
+    )
   }) 
 })
+
+$('.menu-buttons-hamburger').on('click', () =>{
+   $('#menu-buttons-mini').toggle("slow");
+})
+
+$(document).on('click','.menu-button',() =>{
+	console.log('mouse out working')
+   $('#menu-buttons-mini').hide('slow');
+   $('#menu-buttons-hamburger').show();
+})
+
 
 
 
