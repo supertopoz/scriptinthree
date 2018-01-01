ec1b1030fe3c475ced635fa00b384c836a0d01cd
supertopoz <deepsleepsounds@gmail.com>
2018-01-01 09:21:38 +0900

    add redux and menu items from firebase via redux

--- src/app.js
+++ src/app.js
@@ -13,49 +13,39 @@ let menu = [
 ]
 
 
-/*// Reducer - takes the action types and reduces them into a new state condition.
+// Reducer - takes the action types and reduces them into a new state condition.
 const text = (state = {}, actions) =>{
   switch (actions.type) {
-    case 'UPDATE_MENU': return Object.assign({}, state, {value: actions.value})
+    case 'UPDATE_MENU': return Object.assign({}, state, {menuItems: actions.menuItems})
     default: return state
   }  
 }
 
 // Store to hold state of the app - only one store but many properties? 
-const updateText = Redux.createStore(text);
+const updateUi = Redux.createStore(text);
 
 
 //Use subscribe() to update the UI in response to state changes.
-updateText.subscribe(() => {
-  let text = updateText.getState().value
-  localStorage.setItem("text",text)
-  $('#display').text(text)
+updateUi.subscribe(() => {
+  let data = updateUi.getState().menuItems
+  createMenu(data)
 });
 
 
-// The only way to mutate the internal state is to dispatch an action.
-// Dispatch = "Please dispatch this change, please"
-$('#square').on('keyup',(e) => updateText.dispatch({type:'UPDATE_TEXT','value':e.target.value}))
-
-$(document).ready(()=>{
-  console.log('working')
-  let text = localStorage.getItem("text")
-  updateText.dispatch({type:'UPDATE_TEXT','value':text})
-})*/
 
 
-
-$('document').ready(()=>{
-
-  menu.forEach(item => {
+var createMenu = (data) => {
+  
+  for (var i in data) {
+  	var text = data[i].title.toUpperCase();
 	$('#menu-buttons').append(
-    '<div class="menu-button">'+item+'</div>'
+    '<div class="menu-button">'+text+'</div>'
     )
     $('#menu-buttons-mini').append(
-    '<div class="menu-button">'+item+'</div>'
+    '<div class="menu-button">'+text+'</div>'
     )
-  }) 
-})
+  }
+}
 
 $('.menu-buttons-hamburger').on('click', () =>{
    $('#menu-buttons-mini').toggle("slow");
@@ -82,7 +72,8 @@ $(document).ready(()=>{
       var db = firebase.database();
       var ref = db.ref("/");
       ref.on("value", function(snapshot) {
-        console.log(snapshot.val());
+        var data = snapshot.val();
+        updateUi.dispatch({type:'UPDATE_MENU','menuItems': data});
       }, function (errorObject) {
       console.log("The read failed: " + errorObject.code);
        });
